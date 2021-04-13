297025e9b0d603641d078d1c3bbe64bfe1ff0705
md umar faruk <mdumar11233>
2021-04-14 03:34:33 +0600

    added new code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij0ac.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -10,6 +11,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
+
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -19,6 +23,7 @@ app.get('/', (req, res) => {
 
 // Database connection
 client.connect(err => {
+    console.log('database connected');
   const appointmentCollection = client.db("doctorsPortal").collection("appointment");
 
   app.post('/addAppointment', (req, res) =>  {
@@ -27,7 +32,30 @@ client.connect(err => {
       .then(result => {
           res.send(result.insertedCount > 0)
       })
+  });
+
+  app.post('/appointmentByDate', (req, res) =>  {
+    const date = req.body;
+    appointmentCollection.find({date: date})
+    .toArray((err, documents) => {
+        res.send(documents)
+    })
+});
+
+  app.post('/addDoctor', (req, res) => {
+      const file = req.files.file;
+      const name = req.body.name;
+      const email = req.body.email;
+      console.log(name, email, file);
+      file.mv(`${__dirname}/doctors/${file.name}`, err =>{
+          if(err){
+              console.log(err);
+              return res.status(500)
+          }
+          return res.send({name: file.name, path: `${file.name}`})
+      })
   })
+
 });
 
 
